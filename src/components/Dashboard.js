cd0018a8d6ffbd1d1e903f74bb105f365dd39b8a
vishal <vishaltaral09@gamil.com>
2022-10-18 15:22:42 +0530

    update in signUp form

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,53 +3,56 @@ import '../style/dashboard.scss'
 
 const Dashboard = () => {
     return (
-        <div>
-            <div className="first">
-                <div className="container custom-button">
-                    <button className="btn" type="button">Add Fields</button>
-                </div>
-                <div className="search container" >
-                    <label><b>Search</b></label>
-                    <input type="text" placeholder="Search here" />
-                </div>
-                <div className="table container">
-                    <table classNameName='table-data' cellspacing="0" cellpadding="5">
-                        <tr>
-                            <th className="table-th">Sr.no <a href="#"><i className="fa fa-sort" aria-hidden="true"></i></a></th>
-                            <th className="table-th">Name Of Employee <a href="#"><i className="fa fa-sort" aria-hidden="true"></i></a></th>
-                            <th className="table-th">Role <a href="#"><i className="fa fa-sort" aria-hidden="true"></i></a></th>
-                            <th className="table-th">Join Date <a href="#"><i className="fa fa-sort" aria-hidden="true"></i></a></th>
-                            <th className="table-th">Experience <a href="#"><i className="fa fa-sort" aria-hidden="true"></i></a></th>
-                            <th className="table-th">Salary <a href="#"><i className="fa fa-sort" aria-hidden="true"></i></a></th>
-                            <th className="table-th">Gender <a href="#"><i className="fa fa-sort" aria-hidden="true"></i></a></th>
-                            <th className="table-th"> Contact no <a href="#"><i className="fa fa-sort" aria-hidden="true"></i></a></th>
-                            <th className="table-th">Address <a href="#"><i className="fa fa-sort" aria-hidden="true"></i></a></th>
-                        </tr>
-                        <tr>
-                            <td className="tableData">Vishal Taral</td>
-                            <td className="tableData">Web Developer</td>
-                            <td className="tableData">26 <sup>th</sup> September</td>
-                            <td className="tableData">1 month</td>
-                            <td className="tableData">15,000</td>
-                            <td className="tableData">male</td>
-                            <td className="tableData">9176000080</td>
-                            <td className="tableData">balaji nagar, nerhe 411041</td>
-                        </tr>
-                        <tr>
-                            <td className="tableData">02</td>
-                            <td className="tableData">Rohan Odel</td>
-                            <td className="tableData">Web Developer</td>
-                            <td className="tableData">26<sup>th</sup> February</td>
-                            <td className="tableData">9 month</td>
-                            <td className="tableData">30,000</td>
-                            <td className="tableData">male</td>
-                            <td className="tableData">9168454580</td>
-                            <td className="tableData">katraj</td>
-                        </tr>
-                    </table>
-                </div>
-            </div>
+        <div class="first">
+        <div class="container custom-button">
+            <button class="btn" type="button">Add Fields</button>
         </div>
+    
+        <div class="search container" >
+            <label><b>Search</b></label>
+            <input type="text" placeholder="Search here"/>
+        </div>
+        
+        
+        <div class="table container">
+            
+            <table cellspacing="0" cellpadding="5">
+                <tr>
+                    <th class="tableData">Sr.no <a href="#"><i class="fa fa-sort" aria-hidden="true"></i></a></th>
+                    <th class="tableData">Name Of Employee <a href="#"><i class="fa fa-sort" aria-hidden="true"></i></a></th>
+                    <th class="tableData">Position <a href="#"><i class="fa fa-sort" aria-hidden="true"></i></a></th>
+                    <th class="tableData">Join Date <a href="#"><i class="fa fa-sort" aria-hidden="true"></i></a></th>
+                    <th class="tableData">Experience <a href="#"><i class="fa fa-sort" aria-hidden="true"></i></a></th>
+                    <th class="tableData">Salary <a href="#"><i class="fa fa-sort" aria-hidden="true"></i></a></th>
+                    <th class="tableData">Values <a href="#"><i class="fa fa-sort" aria-hidden="true"></i></a></th>
+                    <th class="tableData">Phone no <a href="#"><i class="fa fa-sort" aria-hidden="true"></i></a></th>
+                    <th class="tableData">Address <a href="#"><i class="fa fa-sort" aria-hidden="true"></i></a></th>
+                </tr>
+                <tr>
+                    <td class="tableData">01</td>
+                    <td class="tableData">Vishal Taral</td>
+                    <td class="tableData">Web Developer</td>
+                    <td class="tableData">26 <sup>th</sup>September</td>
+                    <td class="tableData">1 month</td>
+                    <td class="tableData">15,000</td>
+                    <td class="tableData">male</td>
+                    <td class="tableData">9176000080</td>
+                    <td class="tableData">balaji nagar, nerhe 411041</td>
+                </tr>
+                <tr>
+                    <td class="tableData">02</td>
+                    <td class="tableData">Rohan Odel</td>
+                    <td class="tableData">Web Developer</td>
+                    <td class="tableData">26 <sup>th</sup>February</td>
+                    <td class="tableData">9 month</td>
+                    <td class="tableData">30,000</td>
+                    <td class="tableData">male</td>
+                    <td class="tableData">9168454580</td>
+                    <td class="tableData">katraj</td>
+                </tr>
+            </table>
+        </div>
+    </div>
     )
 }
 
