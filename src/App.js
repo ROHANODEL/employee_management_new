f43fcecae637b37cd0e26553297b8721c449e0dc
Rohan odel <rohan.odel@polygolts.io>
2022-10-19 19:32:15 +0530

    add emp

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Protected from './components/Protected';
 import {Provider} from 'react-redux';
 import {store} from './store';
 import AdminDashboard from './components/admin/components/AdminDashboard';
+import AddEmp from './components/admin/components/AddEmp';
 
 function App() {
   return (
@@ -14,8 +15,9 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Protected Component={AdminDashboard}/>} />
-          <Route path='/login' element=<Login/> />
+          <Route path='/login' element={<Login/>} />
           <Route path='signup' element={<Protected Component={SignUp}/>} />
+          <Route path='/addemp' element={<AddEmp/>} />
         </Routes>
       </BrowserRouter>
       </Provider>
